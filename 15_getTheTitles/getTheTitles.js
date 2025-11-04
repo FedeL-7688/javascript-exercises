eaca034fea5444650a053bd826d62f3b61e256b8
FedeL <115208276+FedeL-7688@users.noreply.github.com>
2025-11-04 12:03:37 -0300

    finished 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
 const getTheTitles = function() {
-
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+    
+    titlearray = []
+    for (i= 0;i<books.length;i++){
+        titlearray.push(books[i].title)
+    }
+    return titlearray
 };
-
+getTheTitles()
 // Do not edit below this line
 module.exports = getTheTitles;
