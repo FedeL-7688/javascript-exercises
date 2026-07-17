db2b3e4a7dc8c89e6ae18aa40b6e3b37e7a2a130
Fede <115208276+FedeL-7688@users.noreply.github.com>
2026-07-17 17:20:06 -0300

    finished first 2 exercises
    -Finished factorial function
    -Semi-Finished object value searcher function
      -some fix must be made to pass all tests required by the exercise

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,9 @@
-const factorial = function() {
-
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  } else if (n < 0 || n % 1 !== 0 || typeof n !== "number") {
+    return undefined;
+  } else return n * factorial(n - 1);
 };
-
 // Do not edit below this line
 module.exports = factorial;
--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,24 @@
-const contains = function() {
-  
+const test1 = {
+  try: 44,
 };
+
+const contains = function (obj, val) {
+  if (typeof obj !== "object") return false
   
+  objArr = Object.values(obj);
+
+  if (objArr.includes(val)) return true;
+  else
+    for (const key of objArr) {
+      if (typeof key === "object") {
+        if( contains(key, val)===true)return true;
+       
+      }
+    }
+
+  return false;
+};
+
+console.log(contains(test1, 44));
 // Do not edit below this line
 module.exports = contains;
